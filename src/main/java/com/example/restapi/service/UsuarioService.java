8effece97f390a72525b1c37c5dc6cf8c2297337
EkHi04 <160221030+EkHi04@users.noreply.github.com>
2025-03-26 21:39:53 +0100

    Crear controller y service de Usuario

---
+++ src/main/java/com/example/restapi/service/UsuarioService.java
@@ -0,0 +1,14 @@
+package com.example.restapi.service;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import es.uDeusto.G15procesos.entity.Usuario;
+
+public class UsuarioService {
+    private final Map<Long, Usuario> usuarios = new HashMap<>();
+    
+    
+
+
+}

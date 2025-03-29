4674b440e72c10c423133c3d78565b85e7300cb1
EkHi04 <160221030+EkHi04@users.noreply.github.com>
2025-03-29 18:00:31 +0100

    Arreglo de repository

--- src/main/java/com/example/restapi/repository/CocheRepository.java
+++ src/main/java/com/example/restapi/repository/CocheRepository.java
@@ -4,5 +4,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-    public interface CocheRepository extends JpaRepository<Coche, Long> {
+    public interface CocheRepository extends JpaRepository<Coche, String> {
 }

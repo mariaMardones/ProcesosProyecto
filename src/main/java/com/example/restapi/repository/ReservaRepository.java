5397920d9a5cc3142ccd8985303cd643bd4a0377
EkHi04 <160221030+EkHi04@users.noreply.github.com>
2025-03-29 19:06:20 +0100

    reserva repository cambio

--- src/main/java/com/example/restapi/repository/ReservaRepository.java
+++ src/main/java/com/example/restapi/repository/ReservaRepository.java
@@ -4,5 +4,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-    public interface ReservaRepository extends JpaRepository<Reserva, String> {
+    public interface ReservaRepository extends JpaRepository<Reserva, Integer> {
 }

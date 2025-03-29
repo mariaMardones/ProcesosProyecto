b523f519ba573e2ce8f9602cec2ca4a0ab6249ab
EkHi04 <160221030+EkHi04@users.noreply.github.com>
2025-03-29 17:46:34 +0100

    Usuario cerrar sesion

--- src/main/java/com/example/restapi/controller/UsuarioController.java
+++ src/main/java/com/example/restapi/controller/UsuarioController.java
@@ -1,5 +1,6 @@
 package com.example.restapi.controller;
 
 public class UsuarioController {
+	
 
 }
--- src/main/java/com/example/restapi/service/UsuarioService.java
+++ src/main/java/com/example/restapi/service/UsuarioService.java
@@ -1,13 +1,40 @@
 package com.example.restapi.service;
 
+import java.sql.Timestamp;
+import java.time.Instant;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Optional;
 
 import com.example.restapi.model.Usuario;
 
 public class UsuarioService {
     private final Map<Long, Usuario> usuarios = new HashMap<>();
     public static Map<String, Usuario> tokens = new HashMap<>();
+    
+    public String generarToken(Usuario u) {
+        String token = Timestamp.from(Instant.now()).toString();
+        tokens.put(token, u);
+        return token;
+    }
+
+    public Optional<Boolean> logout(String token) {
+        if (tokens.containsKey(token)) {
+            tokens.remove(token);
+            return Optional.of(true);
+        } else {
+            return Optional.empty();
+        }
+
+    }
+        
+    public static Map<String, Usuario> getTokens() {
+        return tokens;
+    }
+
+    public Map<String, Usuario> obtenerTokens() {
+        return tokens;
+    }
     
     
 
